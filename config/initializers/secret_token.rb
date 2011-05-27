b2ae2ead671db7447ae8e60ec1665cb2e24b1558
Karl Grzeszczak <karlgrz@gmail.com>
2011-05-27 01:00:57 -0500

    default rails site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monstermash::Application.config.secret_token = 'fd1b54db60bd65932ba391387110eae3247d11e526c131856a89111005470c16b964e8c70acb350cb4706e4b3d7d7c2db49b8f444fc43ea7b58ff82feabaff30'
